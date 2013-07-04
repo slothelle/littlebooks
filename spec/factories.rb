e502675fceaf7ce4971284d075c1bbddc3c82468
Liz Abinante <eabinante@gmail.com>
2013-07-04 16:35:10 -0500

    updated user information to pass validations; created factory

---
+++ spec/factories.rb
@@ -0,0 +1,7 @@
+FactoryGirl.define do
+  factory :user do
+    name     "Joe"
+    email    "joe@joe.com"
+    password "password1234"
+  end
+end
--- spec/support/login.rb
+++ spec/support/login.rb
@@ -2,8 +2,8 @@ module LoginHelper
   def sign_in
     FactoryGirl.create(:user)
     visit login_path
-    fill_in("Username", :with => "niles")
-    fill_in("Password", :with => "1234")
+    fill_in("Email", :with => "joe@joe.com")
+    fill_in("Password", :with => "password1234")
     click_button("Login")
   end
 end
