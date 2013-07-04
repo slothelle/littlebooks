a114af9c0a5ac993a6514a545f86c9c3063ce70a
Liz Abinante <eabinante@gmail.com>
2013-07-04 16:35:51 -0500

    changed path for login form

--- spec/support/login.rb
+++ spec/support/login.rb
@@ -1,7 +1,7 @@
 module LoginHelper
   def sign_in
     FactoryGirl.create(:user)
-    visit login_path
+    visit root_path
     fill_in("Email", :with => "joe@joe.com")
     fill_in("Password", :with => "password1234")
     click_button("Login")
