2fd814be21e6018a2d1022723c5a995b4d235de2
Liz Abinante <eabinante@gmail.com>
2013-07-04 10:04:23 -0500

    changed redirects for user#create

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,17 +1,16 @@
 class UsersController < ApplicationController
-
   def new
     @user = User.new
+    render "_new"
   end
 
   def create
     @user = User.new(params[:user])
     if @user.save
-      session[:user_id] = @user.id 
-      redirect_to user_path(current_user)
+      session[:user_id] = @user.id
+      redirect_to root_path
     else
-      render new_user_path
+      redirect_to root_path
     end
   end
-
 end
