c51fbb1e2cfc80766decfdffd9b700a4d48cc24f
Liz Abinante <eabinante@gmail.com>
2013-07-04 10:13:05 -0500

    moved current_user to helper module; created method to make new session

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def current_user
+    @current_user ||= session[:current_user_id] && User.find(session[:current_user_id])
+  end
+
+  def create_session
+    session[:current_user_id] = @user.id
+  end
 end
