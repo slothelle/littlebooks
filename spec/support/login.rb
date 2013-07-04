40e8b1c007672f92611b3a8cc1d81e3e8e7b2da9
Liz Abinante <eabinante@gmail.com>
2013-07-04 16:54:23 -0500

    added helper methods for signing up and logging in

--- spec/support/login.rb
+++ spec/support/login.rb
@@ -1,9 +1,17 @@
 module LoginHelper
-  def sign_in
+  def login
     FactoryGirl.create(:user)
-    visit root_path
+    visit new_session_path
     fill_in("Email", :with => "joe@joe.com")
     fill_in("Password", :with => "password1234")
     click_button("Login")
+  end
+
+  def signup
+    visit new_user_path
+    fill_in("Name", :with => "Joe")
+    fill_in("Email", :with => "joe@joe.com")
+    fill_in("Password", :with => "password1234")
+    click_button("Signup")
   end
 end
