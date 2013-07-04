055102d4eb7ef671d932387c612a462c5d6c6edc
Liz Abinante <eabinante@gmail.com>
2013-07-04 16:30:46 -0500

    outline of user integration tests

---
+++ spec/requests/user_spec.rb
@@ -0,0 +1,17 @@
+require 'spec_helper'
+
+describe "Users can signup, login, and logout" do
+  context "on the home page" do
+    it "logs in the user with correct credentials" do
+      pending
+    end
+
+    it "creates a user given valid inputs" do
+      pending
+    end
+
+    it "logs an existing user out" do
+      pending
+    end
+  end
+end
