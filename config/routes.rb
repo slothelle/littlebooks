d9105022a0228de2157994675041e318322bf059
Liz Abinante <eabinante@gmail.com>
2013-07-04 09:22:19 -0500

    added routes for sessions, login, logout, signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 LittleBooks::Application.routes.draw do
   resources :users
+  resources :sessions, only: [:new, :create, :destroy]
+
+  match '/signup',  to: 'users#new'
+  match '/login',   to: 'sessions#new'
+  match '/logout',  to: 'sessions#destroy', via: :delete
+
+  root :to => 'high_voltage/pages#show', :id => 'home'
 end
