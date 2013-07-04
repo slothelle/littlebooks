f0b2f8af3a747c0950b47ba258e57175beacc592
dbc-apprentice <apprentice@devbootcamp.com>
2013-07-03 21:45:49 -0500

    built current user helper, deleted base rails index view

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  before_filter :current_user
+
+  def current_user
+    @current_user ||= User.find_by_id(session[:user_id])
+  end
 end
