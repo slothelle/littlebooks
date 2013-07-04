b3b77e569ad002f9587bafa09ec0de12ed6e7cda
Nestor Castillo <nes_23@yahoo.com>
2013-07-03 21:06:20 -0500

    created user sign up form and other dependencies on the models and views

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,16 @@
 class UsersController < ApplicationController
 
+  def new
+  end
+
+  def create
+    @user = User.new(params[:user])
+    if @user.save
+      session[:user_id] = @user.id 
+      redirect_to user_path(current_user)
+    else
+      render new_user_path
+    end
+  end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,5 @@ class User < ActiveRecord::Base
   has_many :mytales
   has_many :stories, :through => :mytales
 
+  has_secure_password
 end
