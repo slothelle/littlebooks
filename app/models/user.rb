c0a41ce1d384e9a9b54f5521bd1aeaadffb28e8a
dbc-apprentice <apprentice@devbootcamp.com>
2013-07-04 08:10:27 -0500

    added password length validation to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,5 +11,5 @@ class User < ActiveRecord::Base
   validates_presence_of :name, :email, :password
   validates :email, :format {:with => /\b[A-Z0-9._%a-z\-]+@(?:[A-Z0-9a-z\-]+\.)+[A-Za-z]{2,4}\z/ }
   validates_length_of :name, :within => 4..20, :too_long => "Please choose a name with less than 20 characters", :too_short => "Please choose a name with at least 4 characters"
-  
+  validates_length_of :password, :within => 6..15, :too_long => "Please choose a password with less than 15 characters", :too_short => "Please choose a password with at least 6 characters"
 end
