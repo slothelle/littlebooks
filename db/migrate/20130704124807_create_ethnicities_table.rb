8e34ab99d1a758136d4dc6ad46f0a6c0b32d18c1
dbc-apprentice <apprentice@devbootcamp.com>
2013-07-04 07:51:46 -0500

    created genders migration, edited ethnicities table

--- db/migrate/20130704124807_create_ethnicities_table.rb
+++ db/migrate/20130704124807_create_ethnicities_table.rb
@@ -1,7 +1,7 @@
 class CreateEthnicitiesTable < ActiveRecord::Migration
   def change
     create_table :ethnicities do |t|
-      t.text       :group
+      t.string      :group
       t.timestamps
     end
   end
