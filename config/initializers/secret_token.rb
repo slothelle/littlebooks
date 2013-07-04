827fc2ac46fd54d4cb831db1380979060bf08906
dbc-apprentice <apprentice@devbootcamp.com>
2013-07-03 19:24:49 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LittleBooks::Application.config.secret_token = 'c3321887e641d54d8d5835ce87e1ef98e9479b15b9cac6a65effbb127386212b3f75f0a1c747047e7f4e910c56fc2bfc08754c1d19574aa52bbc4cce516b7b35'
