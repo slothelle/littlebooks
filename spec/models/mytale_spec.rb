8ff5a8f670647ddfbc1ff586dbd27d3fa18ec786
Liz Abinante <eabinante@gmail.com>
2013-07-05 15:02:17 -0500

    updated mytale tests to match new validations

--- spec/models/mytale_spec.rb
+++ spec/models/mytale_spec.rb
@@ -1,11 +1,10 @@
 require 'spec_helper'
 
-describe Mytale do 
+describe Mytale do
 
   it { should be_instance_of(Mytale) }
   it { should belong_to(:user) }
   it { should belong_to(:story) }
   it { should validate_presence_of(:content) }
-  it { should validate_presence_of(:summary) }
 
 end
