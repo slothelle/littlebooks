07aa9abe48662f0766364f58cbd6c2126fe8cd9e
Liz Abinante <eabinante@gmail.com>
2013-07-05 15:04:49 -0500

    updated user spec to match new validations and use factories

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,13 +6,13 @@ describe User do
   it { should have_many(:mytales) }
   it { should have_many(:stories).through(:mytales) }
   it { should validate_presence_of(:name) }
-  it { should ensure_length_of(:name).is_at_least(4).with_short_message(/Please choose a name with at least 4 characters/) }
+  it { should ensure_length_of(:name).is_at_least(2).with_short_message(/Please choose a name with at least 4 characters/) }
   it { should ensure_length_of(:name).is_at_most(20).with_long_message(/Please choose a name with less than 20 characters/) }
   it { should validate_presence_of(:email) }
   it { should validate_uniqueness_of(:email) }
   it { should validate_presence_of(:password) }
-  it { should ensure_length_of(:password).is_at_least(6).with_short_message(/Please choose a password with at least 6 characters/) }
-  it { should ensure_length_of(:password).is_at_most(15).with_long_message(/Please choose a password with less than 15 characters/) }
+  it { should ensure_length_of(:password).is_at_least(6).with_short_message(/must be at least 6 characters/) }
+  it { should ensure_length_of(:password).is_at_most(15).with_long_message(/must be less than 15 characters/) }
 
   it { should_not allow_value("blah").for(:email) }
   it { should allow_value("little@littlebooks.com").for(:email) }
@@ -20,28 +20,28 @@ describe User do
   it { should allow_mass_assignment_of(:email) }
   it { should allow_mass_assignment_of(:password) } #we shouldn't allow mass assignment to password?
 
-  context 'with valid input' do 
+  context 'with valid input' do
 
-    it 'should create a user' do 
-      person = User.create(name: "Jackie Awesome", email: "jackie@awesome", password: "123Ab3cd")
+    it 'should create a user' do
+      person = FactoryGirl.create(:user)
       expect(person).to be_instance_of(User)
     end
 
   end
 
-  context 'with invalid input' do 
+  context 'with invalid input' do
 
-    it 'should not create a user without a name' do 
+    it 'should not create a user without a name' do
       person = User.create(email: "jackie@awesome", password: "123Ab3cd")
       expect(person.id).to be(nil)
     end
 
-    it 'should not create a user without an email' do 
+    it 'should not create a user without an email' do
       person = User.create(name: "Jackiew Awesome", password: "123Ab3cd")
       expect(person.id).to be(nil)
     end
 
-    it 'should not create a usser without a password' do 
+    it 'should not create a usser without a password' do
       person = User.create(name: "Jackiew Awesome", email: "jackie@awesome")
       expect(person.id).to be(nil)
     end
