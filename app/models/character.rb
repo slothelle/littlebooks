41d8661c44475aa6bf42867e242d5ffacc20ec0a
Liz Abinante <eabinante@gmail.com>
2013-07-06 12:17:45 -0500

    created character model with validations and relationships; all tests passing

--- app/models/character.rb
+++ app/models/character.rb
@@ -3,4 +3,7 @@ class Character < ActiveRecord::Base
   belongs_to :story
   belongs_to :gender
   has_many :mytale_characters
+
+  validates_presence_of :name, :story, :gender
+  validates_length_of :name, :within => 2..20, :too_long => "must be less than 20 characters.", :too_short => "must be at least 2 characters."
 end
