243fd9cc347c22c73e0c4cee0fedbe1ad4abb3bd
Liz Abinante <eabinante@gmail.com>
2013-07-06 12:13:59 -0500

    added validations to ethnicities model; all tests passing

--- spec/models/ethnicity_spec.rb
+++ spec/models/ethnicity_spec.rb
@@ -1,8 +1,7 @@
 require 'spec_helper'
 
 describe Ethnicity do
-
-it { should be_instance_of(Ethnicity) }
-it { should have_many(:people_images) }
-
+  it { should be_instance_of(Ethnicity) }
+  it { should have_many(:people_images) }
+  it { should validate_presence_of(:group) }
 end
