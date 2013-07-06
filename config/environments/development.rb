c713ff29551caa2bc508536d34bba0be6d8d1b6b
Nestor Castillo <nes_23@yahoo.com>
2013-07-06 11:09:09 -0500

    setting up action mailer settings for dev environment

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -14,7 +14,19 @@ LittleBooks::Application.configure do
   config.action_controller.perform_caching = false
 
   # Don't care if the mailer can't send
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
+
+  #Change mail delivery to smtp:
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 587,
+    domain: "littlebooks.herokuapp.com",
+    authentication: "plain",
+    enable_starttls_auto: true,
+    user_name: "littlebooksnoreply@gmail.com"
+    password: "Devb00tcamp!"
+  }
 
   # Print deprecation notices to the Rails logger
   config.active_support.deprecation = :log
