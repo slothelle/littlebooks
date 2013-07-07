d71c660b66ef73518f5842ca4db8bb50165751d0
Nestor Castillo <nes_23@yahoo.com>
2013-07-07 14:46:54 -0500

    migrations to drop ethnicity table and two new columns to story table

---
+++ db/migrate/20130707194044_upate_stories.rb
@@ -0,0 +1,11 @@
+class UpateStories < ActiveRecord::Migration
+  def up
+    add_column :stories, :source_link, :string
+    add_column :stories, :attribution, :string
+  end
+
+  def down
+    remove_column :stories, :source_link
+    remove_column :stories, :attribution
+  end
+end
