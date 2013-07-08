3db35467b2b1780937ba5852f8817a11459f64b1
Liz Abinante <eabinante@gmail.com>
2013-07-08 10:08:40 -0500

    created view and route for mytale_images#new

--- app/controllers/mytale_images_controller.rb
+++ app/controllers/mytale_images_controller.rb
@@ -2,6 +2,6 @@ class MytaleImagesController < ApplicationController
   def new
     @peopleimages = PeopleImage.all
     @mytale = Mytale.find_by_id(params[:mytale_id])
-    @mycharacter = MytalesCharacter.find_by_id(@mytale.mytales_characer_id)
+    @mycharacter = MytalesCharacter.find_by_id(@mytale.mytales_character_id)
   end
 end
