ddde5fe254088ba14bbcae9af94e210e8fee6532
Nestor Castillo <nes_23@yahoo.com>
2013-07-08 12:29:52 -0500

    added more config to support fb login

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,11 @@ LittleBooks::Application.routes.draw do
   resources :users
   resources :sessions, only: [:new, :create, :destroy]
 
+  match '/auth/facebook/callback', :to => 'sessions#create'
+  match '/auth/failure', :to => redirect('/')
+  #do we need this since we already have the delete route below?
+  #match '/logout', :to => 'sessions#destroy'
+
   delete '/logout', to: 'sessions#destroy'
 
   root :to => 'stories#index', :id => 'home'
