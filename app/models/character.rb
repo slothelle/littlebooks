87940cac11fd4e6ae61fa3287f15037c0398c869
Liz Abinante <eabinante@gmail.com>
2013-07-08 09:28:17 -0500

    updated validations and relationships to reflect tests correctly

--- app/models/character.rb
+++ app/models/character.rb
@@ -4,6 +4,6 @@ class Character < ActiveRecord::Base
   belongs_to :gender
   has_many :mytales_characters
 
-  validates_presence_of :name, :story, :gender
+  validates_presence_of :name, :story_id, :gender_id
   validates_length_of :name, :within => 2..20, :too_long => "must be less than 20 characters.", :too_short => "must be at least 2 characters."
 end
