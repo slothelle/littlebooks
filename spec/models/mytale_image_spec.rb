7f5482278126e786e088d3d35b886cffbec9c3df
Liz Abinante <eabinante@gmail.com>
2013-07-08 13:43:25 -0500

    updated specs for mytale_images and story models to reflect new relationships and validations

--- spec/models/mytale_image_spec.rb
+++ spec/models/mytale_image_spec.rb
@@ -4,6 +4,8 @@ describe MytaleImage do
   it { should be_instance_of(MytaleImage) }
   it { should belong_to(:mytale) }
   it { should belong_to(:people_image) }
-  it { should validate_presence_of(:mytale) }
-  it { should validate_presence_of(:people_image) }
+  it { should validate_presence_of(:mytale_id) }
+  it { should validate_presence_of(:people_image_id) }
+  it { should allow_mass_assignment_of(:mytale_id) }
+  it { should allow_mass_assignment_of(:people_image_id) }
 end
