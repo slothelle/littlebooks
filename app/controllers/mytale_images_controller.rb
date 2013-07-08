49c30f0549618acae679abe06506e70c9c1444e6
Liz Abinante <eabinante@gmail.com>
2013-07-08 10:40:57 -0500

    establish create route for mytale_images; created new view for image selection

--- app/controllers/mytale_images_controller.rb
+++ app/controllers/mytale_images_controller.rb
@@ -4,4 +4,13 @@ class MytaleImagesController < ApplicationController
     @mytale = Mytale.find_by_id(params[:mytale_id])
     @mycharacter = MytalesCharacter.find_by_id(@mytale.mytales_character_id)
   end
+
+  def create
+    @myimage = MytaleImage.new(params)
+    if @myimage.save
+      # magic
+    else
+      # poop
+    end
+  end
 end
