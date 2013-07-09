fce59086adf10abe575a67142eaafd4d4fb25dfe
Liz Abinante <eabinante@gmail.com>
2013-07-09 17:40:37 -0500

    refactor the ugliest controllers in the world and now they are awesome

--- app/controllers/mytales_controller.rb
+++ app/controllers/mytales_controller.rb
@@ -1,21 +1,17 @@
 class MytalesController < ApplicationController
   def new
-    @story = Story.find_by_id(params[:story_id])
-    @character = @story.character
+    grab_story_character
+    grab_all_genders
     @mytale = Mytale.new
     @mytales_character = MytalesCharacter.new
-    @gender = Gender.all
   end
 
   def create
-    @story = Story.find_by_id(params[:story_id])
-    @mytales_character = MytalesCharacter.create(name: params[:name], gender: Gender.find(params[:gender]), character: @story.character)
-    params[:mytale][:mytales_character_id] = @mytales_character.id
-    @mytale = Mytale.new(params[:mytale])
-    @mytale.replace_goldilocks
+    grab_story_character
+    create_mytales_character
+    create_mytale
     if @mytale.save
-      @mytales_character.update_attributes(mytale_id: @mytale.id)
-      @mytale.update_attributes(user: current_user)
+      create_mytales_character_relationship
       redirect_to new_story_mytale_mytale_image_path(story_id: @story.id, mytale_id: @mytale.id)
     else
       render :new
@@ -23,25 +19,21 @@ class MytalesController < ApplicationController
   end
 
   def edit
-    @story = Story.find_by_id(params[:story_id])
     @mytale = Mytale.find_by_id(params[:id])
-    @character = @story.character
-    @peopleimages = PeopleImage.all
     @mytales_character = MytalesCharacter.find_by_id(@mytale.mytales_character_id)
-    @gender = Gender.all
+    grab_story_character
+    grab_all_images
+    grab_all_genders
   end
 
   def update
-    @gender = Gender.all
-    @peopleimages = PeopleImage.all
-    @story = Story.find_by_id(params[:story_id])
+    grab_all_genders
+    grab_all_images
+    grab_story_character
     @mytale = Mytale.find_by_id(params[:id])
     @mytales_character = MytalesCharacter.find_by_id(@mytale.mytales_character_id)
-    @mytales_character.assign_attributes(name: params[:name], gender: Gender.find(params[:gender]))
-    @mytale.assign_attributes(params[:mytale])
     @myimage = MytaleImage.find_by_mytale_id(params[:id])
-    @myimage.assign_attributes(people_image_id: params[:image_ids][0].to_i)
-
+    update_all_mytales
     if @mytale.save && @mytales_character.save && @myimage.save
       redirect_to story_mytale_path(story_id: @story.id, mytale_id: @mytale.id)
     else
@@ -51,21 +43,19 @@ class MytalesController < ApplicationController
 
   def show
     @mytale = Mytale.find(params[:id])
-    image = MytaleImage.find_by_id(@mytale.mytale_image_id)
-    @image = PeopleImage.find_by_id(image.people_image_id)
+    find_image
   end
 
   def print
     @mytale = Mytale.find_by_id(params[:mytale_id])
-    image = MytaleImage.find_by_id(@mytale.mytale_image_id)
-    @image = PeopleImage.find_by_id(image.people_image_id)
+    find_image
     render layout: "print"
   end
 
   def read
-    @story = Mytale.find_by_id(params[:mytale_id])
-    @story_paged = @story.page.slice!(params[:slice])
-    @kid = PeopleImage.find_by_id(@story.mytale_image.people_image_id)
+    @mytale = Mytale.find_by_id(params[:mytale_id])
+    @story_paged = @mytale.page.slice!(params[:slice])
+    find_image
     respond_to do |format|
       format.html { render layout: "read" }
       format.js {}
