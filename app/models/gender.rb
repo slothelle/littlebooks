82cf972a41b8a05889ae65fe6ff22321d5cfe4df
Liz Abinante <eabinante@gmail.com>
2013-07-08 19:27:01 -0500

    updated mytales_characters relationship; all gender tests passing now

--- app/models/gender.rb
+++ app/models/gender.rb
@@ -2,6 +2,6 @@ class Gender < ActiveRecord::Base
   attr_accessible :sex
   has_many :people_images
   has_many :characters
-  has_many :mytales_character
+  has_many :mytales_characters
   validates_presence_of :sex
 end
