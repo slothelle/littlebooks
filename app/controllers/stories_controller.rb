c9d0052b3dcbd331810260967c141ae0f216e996
Liz Abinante <eabinante@gmail.com>
2013-07-09 20:19:12 -0500

    updated show and cleaned up index; more to follow

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -1,24 +1,19 @@
 class StoriesController < ApplicationController
   def index
-    if !current_user
-      @story = Story.first
-      @mytale = Mytale.new
-      @character = @story.character
-      @mytales_character = MytalesCharacter.new
-      grab_all_genders
-      @story_paged = @story.paged.slice!(2)
-      render "_new"
-    elsif current_user
-      @story = Story.all
-      @mytale = Mytale.new
-      render "_loggedin"
-    end
+    @stories = Story.all
+    @mytale = Mytale.new
+    @character = @story.character
+    @mytales_character = MytalesCharacter.new
+    grab_all_genders
+    @story_paged = @story.paged.slice!(2)
   end
 
   def show
+    grab_all_genders
     @story = Story.find(params[:id])
-    @story_paged = @story.paged.slice!(params[:slice])
-    render layout: "read"
+    @story_paged = @story.paged.slice!(3)
+    @character = @story.character
+    @mytale = Mytale.new
   end
 end
 
