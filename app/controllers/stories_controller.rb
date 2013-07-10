6d4aed725f655b367c2d894b6c21d2add6711f68
Liz Abinante <eabinante@gmail.com>
2013-07-09 21:09:02 -0500

    cleaned up stories#index and stories#show

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -1,18 +1,13 @@
 class StoriesController < ApplicationController
   def index
     @stories = Story.all
-    @mytale = Mytale.new
-    @character = @story.character
-    @mytales_character = MytalesCharacter.new
     grab_all_genders
-    @story_paged = @story.paged.slice!(2)
   end
 
   def show
     grab_all_genders
     @story = Story.find(params[:id])
-    @story_paged = @story.paged.slice!(3)
-    @character = @story.character
+    @story_paged = @story.paged.slice!(5)
     @mytale = Mytale.new
   end
 end
