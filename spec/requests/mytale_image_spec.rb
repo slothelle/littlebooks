d2fe9dbbcd52832eb1b012feac81c52f03db8977
Liz Abinante <eabinante@gmail.com>
2013-07-10 20:32:50 -0500

    rename file to match conventions; tests passing after making sure the ids line up

--- spec/requests/mytale_image_spec.rb
+++ spec/requests/mytale_image_spec.rb
@@ -1,14 +1,12 @@
 require 'spec_helper'
 
 describe "MytaleImage" do
-  before do
+  it "allows a user to select an image" do
     populate_stories_mytales
     populate_images
-  end
-
-  it "allows a user to select an image", js: true do
-    FactoryGirl.create(:mytale)
-    visit new_story_mytale_mytale_image_path(Story.last.id, Mytale.last.id)
+    story = Story.create(summary: "Summary", content: "Content", title: "Title", character_id: Character.last.id, source_link: "Link", attribution: "attribution")
+    tale = Mytale.create(title: "Title", content: "Content", story_id: Story.last.id, mytales_character_id: MytalesCharacter.last.id)
+    visit new_story_mytale_mytale_image_path(story.id, tale.id)
     link_id = PeopleImage.last.id
     click_link(link_id)
     expect(page).to have_content('Read story now!')
