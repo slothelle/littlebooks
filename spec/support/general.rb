efb43446091779e936f7cac8ad7daac6dc276de6
Liz Abinante <eabinante@gmail.com>
2013-07-10 20:58:47 -0500

    updated helper method to reflect new layout of the site; all mytale request specs passing

--- spec/support/general.rb
+++ spec/support/general.rb
@@ -1,6 +1,8 @@
 module GeneralHelper
   def home_filler
-    visit root_path
+    FactoryGirl.create(:background_image)
+    story = Story.create(summary: "Summary", content: "#{'Content' * 200}", title: "Title", character_id: Character.last.id, source_link: "Link", attribution: "attribution", background_image: BackgroundImage.last)
+    visit new_story_mytale_path(Story.last.id)
     fill_in 'mytale[title]', :with => 'Bill Adventures'
     fill_in 'name', :with => 'Hercules'
   end
