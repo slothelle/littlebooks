79e2f99cce41832352379f84d0f856dc3e2cb94e
Liz Abinante <eabinante@gmail.com>
2013-07-10 20:00:28 -0500

    character model tests updated to reflect new validations; all character tests passing

--- spec/models/character_spec.rb
+++ spec/models/character_spec.rb
@@ -11,7 +11,7 @@ describe Character do
   it { should validate_presence_of(:name) }
   it { should validate_presence_of(:gender_id) }
   it { should ensure_length_of(:name).is_at_least(2).with_short_message(/must be at least 2 characters/) }
-  it { should ensure_length_of(:name).is_at_most(20).with_long_message(/must be less than 20 characters/) }
+  it { should ensure_length_of(:name).is_at_most(40).with_long_message(/must be less than 20 characters/) }
 
   it "requires story_id upon update" do
     character = FactoryGirl.create(:character)
