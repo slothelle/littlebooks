75a2a6fcee24f7a3edd1cf3e97bc09ed1f995f97
dbc-apprentice <apprentice@devbootcamp.com>
2013-07-11 14:43:30 -0500

    refactored replace text method

--- app/models/mytale.rb
+++ app/models/mytale.rb
@@ -11,12 +11,23 @@ class Mytale < ActiveRecord::Base
   slice :content, :as => :page, :window => 0, :slice => { :maximum => 175, :complete => /(?<=\.\s)|(?<=\!\s)|(?<=\,\s)|(?<=\'\s)|(?<=\;\s)|(?<=\:\s)/ }
   slice :content, :as => :edit, :window => 0, :slice => { :maximum => 100, :complete => /(?<=\.\s)|(?<=\!\s)/ }
 
-  def replace_goldilocks
-    content.gsub!(/#{story.character.name}/, mytales_character.name)
-    subber = {/\bmiss\b/ => "lad", /\bHer\b/ => "His", /\bherself\b/ => "himself",
-            /\bher\b/ => "his", /\bShe\b/ => "He", /\bshe\b/ => "he", /\bprincess\b/ => "prince", /\bPrincess\b/ => "prince", /\bgirl\b/ => "boy", /\bwoman\b/ => "man" }
+  def replace_text
+    replace_main_char(/#{story.character.name}/, mytales_character.name)
+    subber = subber_goldy
+
     if mytales_character.gender.sex != "female"
-      subber.each {|k,v| content.gsub!(k,v)}
+      subber.each {|female, male| content.gsub!(female, male)}
     end
   end
+
+  def replace_main_char(currentchar, newchar)
+    content.gsub!(/#{currentchar}/, newchar)
+  end
+
+  def subber_goldy
+    {/\bmiss\b/ => "lad", /\bHer\b/ => "His", /\bherself\b/ => "himself",
+            /\bher\b/ => "his", /\bShe\b/ => "He", /\bshe\b/ => "he", /\bprincess\b/ => "prince", /\bgirl\b/ => "boy", /\bPrincess\b/ => "Prince"}
+  end
 end
+
+
